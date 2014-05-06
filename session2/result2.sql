dffe2932b7d3a208e7a7c0c2d086e267de380c06
QWolf <peter.schroten@hotmail.com>
2014-05-06 15:12:37 +0200

    Exercise2.2C

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,7 +1,3 @@
-INSERT INTO F VALUES('ABC','EF',False,'ABC += ABCDEF, dus het is een superkey, dus het is geen schending');
-INSERT INTO F VALUES('E','D',True,'E += ADE, dit is geen superkey en ook niet triviaal, dus het is een schending');
-INSERT INTO F VALUES('D','A',True,'D += AD, dit is geen superkey en ook niet triviaal, dus het is een schending');
-
 INSERT INTO F1 VALUES('D', 'A', False, 'D += AD, dit is een superkey en dus geen schending.');
-INSERT INTO F2 VALUES('ABC', 'EF', False, 'ABC += BCDEF, dus het is een superkey, dus het is geen schending');
-INSERT INTO F2 VALUES('E', 'D' True, 'E+=DE, dit is geen superkey en ook niet triviaal, dus het is een schending');
+INSERT INTO F21 VALUES('BC', 'EF', False, 'BC += BCEF, dus het is een superkey, dus het is geen schending');
+INSERT INTO F22 VALUES('E', 'D', False, 'E+=DE, dit is een superkey en dus geen schending');
