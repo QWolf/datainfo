44f4185af88757fe8e0e3d9c844707453f8cbae6
QWolf <peter.schroten@hotmail.com>
2014-05-06 16:57:01 +0200

    4D Step 4: Shunting

--- session2/exercise3d.sql
+++ session2/exercise3d.sql
@@ -1,13 +1,10 @@
 SELECT DISTINCT p.name 
-FROM Person p
-WHERE EXISTS(
-	SELECT w.mid 
-	FROM Writes w
-	WHERE p.pid = w.pid
-	AND NOT EXISTS(
-		SELECT m.mid
-		FROM Movie m
-		WHERE m.mid = w.mid
-		AND film heeft regisseur'
-	)
+FROM Person p, Writes w
+WHERE p.pid = w.pid
+AND NOT EXISTS(
+	SELECT m.mid
+	FROM Movie m, Directs d, Writes x
+	WHERE m.mid = x.mid
+	AND d.mid = m.mid
+	AND x.pid = p.pid
 );
