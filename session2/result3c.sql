8ffbc9131d8d7668765e5b28abd9ad1dcd453464
QWolf <peter.schroten@hotmail.com>
2014-05-06 16:31:29 +0200

    step 5 Shunting

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,10 +1,8 @@
-SELECT DISTINCT p.name FROM Person p
-WHERE EXISTS(
-	SELECT * FROM Writes w, Movie m
-	WHERE p.pid = w.pid
-	AND m.mid = w.mid
-	AND NOT EXISTS ( --film heeft geen regisseur
-		SELECT d.pid FROM Directs d
-		WHERE d.mid = m.mid
-	)
+SELECT DISTINCT p.name 
+FROM Person p, Writes w, Movie m
+WHERE p.pid = w.pid
+AND m.mid = w.mid
+AND NOT EXISTS ( --film heeft geen regisseur
+	SELECT d.pid FROM Directs d
+	WHERE d.mid = m.mid
 );
