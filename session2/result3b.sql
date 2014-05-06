ee80f3e6bd7ba6adcbed1095b3868f0eff19cdf0
QWolf <peter.schroten@hotmail.com>
2014-05-06 16:02:48 +0200

    Step 2, Shunting rule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,7 +1,6 @@
-SELECT p.pid FROM Person p
-WHERE EXISTS(
-SELECT DISTINCT * FROM Acts a, Movie m
+SELECT DISTINCT p.pid 
+FROM Person p, Acts a, Movie m 
 WHERE a.pid = p.pid
-AND m.mid = a.mid 
+AND m.mid = a.mid
 AND m.name = 'Back to the Future'
-);
+;
