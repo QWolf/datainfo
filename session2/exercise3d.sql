d8ffd3bc57594187ba511ffcaee7a863ee03f177
QWolf <peter.schroten@hotmail.com>
2014-05-06 16:42:42 +0200

    4D Step 2: Building

--- session2/exercise3d.sql
+++ session2/exercise3d.sql
@@ -1,3 +1,10 @@
-SELECT p.name
+SELECT DISTINCT p.name 
 FROM Person p
-WHERE persoon heeft alleen ongeregisseerde films
+WHERE EXISTS(
+	SELECT w.mid 
+	FROM Writes w
+	WHERE p.pid = w.pid
+	AND NOT EXISTS(
+	'film met regisseur'
+	)
+);
